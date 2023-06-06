646ffc4d7a72fe1d7ba8399a74063be5182aee4f
Deana <demaisja@gmail.com>
2023-06-05 17:06:46 -0700

    js files added, navbar works now in mobile and desktop

---
+++ projectGeneration.js
@@ -0,0 +1,12 @@
+function PENGenerate() {
+  let projectPic = document.createElement("img");
+  let title = document.createElement("h3");
+  let subtitle = document.createElement("P");
+
+  projectPic.src = "deana.jpg";
+  projectPic.alt = "Screenshot of the Portland ESL Network Website";
+  title.textContent = "Portland ESL Network Website";
+  subtitle.textContent = "Volunteer website development using WordPress";
+}
+function GermanGenerate() {}
+function MacawGenerate() {}
