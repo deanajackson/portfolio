d07cbeebbd9108d77e05cbdc30bd1e52df6fbb91
Deana <demaisja@gmail.com>
2023-06-06 22:09:27 -0700

    Post accessibility and code validation check edits

--- projectGeneration.js
+++ projectGeneration.js
@@ -16,16 +16,22 @@ function PENGenerate() {
   let title = document.createElement("h3");
   let subtitle = document.createElement("p");
   let githubButton = document.createElement("button");
+  let ghAnchor = document.createElement("a");
   let siteButton = document.createElement("button");
+  let siteAnchor = document.createElement("a");
 
   projectPic.src = "newpen.png";
   projectPic.alt = "Screenshot of the Portland ESL Network Website";
   title.textContent = "Portland ESL Network Website";
   subtitle.textContent = "Volunteer website development using WordPress";
-  githubButton.textContent = "GitHub Repo";
-  githubButton.onclick = "https://github.com/stephr3/portland-esl-network";
-  siteButton.textContent = "Live Site";
-  siteButton.onclick = "https://portlandesl.com/";
+  githubButton.className = "customButtons";
+  ghAnchor.href = "https://github.com/stephr3/portland-esl-network";
+  ghAnchor.textContent = "GitHub Repo";
+  githubButton.append(ghAnchor);
+  siteButton.className = "customButtons";
+  siteAnchor.textContent = "Live Site";
+  siteAnchor.href = "https://portlandesl.com/";
+  siteButton.append(siteAnchor);
 
   container.append(projectPic);
   container.append(title);
@@ -41,17 +47,23 @@ function GermanGenerate() {
   let title = document.createElement("h3");
   let subtitle = document.createElement("p");
   let githubButton = document.createElement("button");
+  let ghAnchor = document.createElement("a");
   let siteButton = document.createElement("button");
+  let siteAnchor = document.createElement("a");
 
   projectPic.src = "levelupgerman.png";
   projectPic.alt = "Screenshot of the Level Up German Website Homepage";
   title.textContent = "Level Up German";
   subtitle.textContent = "Volunteer website development using WordPress";
-  githubButton.textContent = "GitHub Repo";
-  githubButton.onclick =
+  githubButton.className = "customButtons";
+  ghAnchor.href = "https://github.com/deanajackson/germansite";
+  ghAnchor.textContent = "GitHub Repo";
+  githubButton.append(ghAnchor);
+  siteButton.className = "customButtons";
+  siteAnchor.textContent = "Live Site";
+  siteAnchor.href =
     "http://deanajackson.webhostingforstudents.com/level-up-german/index.html";
-  siteButton.textContent = "Live Site";
-  siteButton.onclick = "https://github.com/deanajackson/germansite";
+  siteButton.append(siteAnchor);
 
   container.append(projectPic);
   container.append(title);
@@ -67,17 +79,23 @@ function MacawGenerate() {
   let title = document.createElement("h3");
   let subtitle = document.createElement("p");
   let githubButton = document.createElement("button");
+  let ghAnchor = document.createElement("a");
   let siteButton = document.createElement("button");
+  let siteAnchor = document.createElement("a");
 
   projectPic.src = "macaws.png";
   projectPic.alt = "Screenshot of Hyacynth Macaw Website homepage";
   title.textContent = "Hyacynth Macaw - Project Intermediate CSS class";
   subtitle.textContent =
     "This was a project for an intermediate CSS class I took at PCC";
-  githubButton.textContent = "GitHub Repo";
-  githubButton.onclick = "https://github.com/deanajackson/macaws";
-  siteButton.textContent = "Live Site";
-  siteButton.onclick = "http://deanajackson.webhostingforstudents.com/macaw/";
+  githubButton.className = "customButtons";
+  ghAnchor.href = "https://github.com/deanajackson/macaws";
+  ghAnchor.textContent = "GitHub Repo";
+  githubButton.append(ghAnchor);
+  siteButton.className = "customButtons";
+  siteAnchor.textContent = "Live Site";
+  siteAnchor.href = "http://deanajackson.webhostingforstudents.com/macaw/";
+  siteButton.append(siteAnchor);
 
   container.append(projectPic);
   container.append(title);
